d3c622e1ca4f541487750aafcacfa60a6c9cc7bf
Erathriel <geoffreynoro@orange.fr>
2019-12-31 16:03:09 +0100

    version du script avec un for

--- init_project.sh
+++ init_project.sh
@@ -4,232 +4,265 @@
 #pour qu'ils soient non vide
 
 tabRep=('src' 'bin' 'build' 'doc' 'include' 'lib' 'tests')
-tabFile=('main.cpp' 'initBin' 'initBuild' 'initDoc' 'initInclude' 'initLib' 'initTest')
+tabFile=('main.cpp' 'initBin.txt' 'initBuild.txt' 'initDoc.txt' 'initInclude.txt' 'initLib.txt' 'initTest.txt')
 
 function createRepFile(){
-	rep=src
-	file=main.cpp
+	# rep=src
+	# file=main.cpp
 
-	if [ -d $rep ]; then
-		echo "$rep existe..."
-		cd $rep
-		echo "rep : `pwd`"
-		if ! [ -e $file ]; then
+	# if [ -d $rep ]; then
+	# 	echo "$rep existe..."
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	if ! [ -e $file ]; then
+	# 		echo "$file inexistant..."
+	# 		echo "creation de $file..."
+	# 		touch $file
+	# 		echo "$file creer"
+	# 	else
+	# 		echo "$file deja creer"
+	# 	fi
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# else
+	# 	echo "$rep inexistant..."
+	# 	echo "creation de $rep..."
+	# 	mkdir $rep
+	# 	echo "$rep creer"
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	echo "$file inexistant..."
+	# 	echo "creation de $file..."
+	# 	touch $file
+	# 	echo "$file creer"
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# fi
+
+	# rep=bin
+	# file=initBin.txt
+
+	# if [ -d $rep ]; then
+	# 	echo "$rep existe..."
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	if ! [ -e $file ]; then
+	# 		echo "$file inexistant..."
+	# 		echo "creation de $file..."
+	# 		touch $file
+	# 		echo "$file creer"
+	# 	else
+	# 		echo "$file deja creer"
+	# 	fi
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# else
+	# 	echo "$rep inexistant..."
+	# 	echo "creation de $rep..."
+	# 	mkdir $rep
+	# 	echo "$rep creer"
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	echo "$file inexistant..."
+	# 	echo "creation de $file..."
+	# 	touch $file
+	# 	echo "$file creer"
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# fi
+
+	# rep=build
+	# file=initBuild.txt
+
+	# if [ -d $rep ]; then
+	# 	echo "$rep existe..."
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	if ! [ -e $file ]; then
+	# 		echo "$file inexistant..."
+	# 		echo "creation de $file..."
+	# 		touch $file
+	# 		echo "$file creer"
+	# 	else
+	# 		echo "$file deja creer"
+	# 	fi
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# else
+	# 	echo "$rep inexistant..."
+	# 	echo "creation de $rep..."
+	# 	mkdir $rep
+	# 	echo "$rep creer"
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	echo "$file inexistant..."
+	# 	echo "creation de $file..."
+	# 	touch $file
+	# 	echo "$file creer"
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# fi
+
+	# rep=doc
+	# file=initDoc.txt
+
+	# if [ -d $rep ]; then
+	# 	echo "$rep existe..."
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	if ! [ -e $file ]; then
+	# 		echo "$file inexistant..."
+	# 		echo "creation de $file..."
+	# 		touch $file
+	# 		echo "$file creer"
+	# 	else
+	# 		echo "$file deja creer"
+	# 	fi
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# else
+	# 	echo "$rep inexistant..."
+	# 	echo "creation de $rep..."
+	# 	mkdir $rep
+	# 	echo "$rep creer"
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	echo "$file inexistant..."
+	# 	echo "creation de $file..."
+	# 	touch $file
+	# 	echo "$file creer"
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# fi
+
+	# rep=include
+	# file=initInclude.txt
+
+	# if [ -d $rep ]; then
+	# 	echo "$rep existe..."
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	if ! [ -e $file ]; then
+	# 		echo "$file inexistant..."
+	# 		echo "creation de $file..."
+	# 		touch $file
+	# 		echo "$file creer"
+	# 	else
+	# 		echo "$file deja creer"
+	# 	fi
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# else
+	# 	echo "$rep inexistant..."
+	# 	echo "creation de $rep..."
+	# 	mkdir $rep
+	# 	echo "$rep creer"
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	echo "$file inexistant..."
+	# 	echo "creation de $file..."
+	# 	touch $file
+	# 	echo "$file creer"
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# fi
+
+	# rep=lib
+	# file=initLib.txt
+
+	# if [ -d $rep ]; then
+	# 	echo "$rep existe..."
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	if ! [ -e $file ]; then
+	# 		echo "$file inexistant..."
+	# 		echo "creation de $file..."
+	# 		touch $file
+	# 		echo "$file creer"
+	# 	else
+	# 		echo "$file deja creer"
+	# 	fi
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# else
+	# 	echo "$rep inexistant..."
+	# 	echo "creation de $rep..."
+	# 	mkdir $rep
+	# 	echo "$rep creer"
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	echo "$file inexistant..."
+	# 	echo "creation de $file..."
+	# 	touch $file
+	# 	echo "$file creer"
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# fi
+
+	# rep=tests
+	# file=initTest.txt
+
+	# if [ -d $rep ]; then
+	# 	echo "$rep existe..."
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	if ! [ -e $file ]; then
+	# 		echo "$file inexistant..."
+	# 		echo "creation de $file..."
+	# 		touch $file
+	# 		echo "$file creer"
+	# 	else
+	# 		echo "$file deja creer"
+	# 	fi
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# else
+	# 	echo "$rep inexistant..."
+	# 	echo "creation de $rep..."
+	# 	mkdir $rep
+	# 	echo "$rep creer"
+	# 	cd $rep
+	# 	echo "rep : `pwd`"
+	# 	echo "$file inexistant..."
+	# 	echo "creation de $file..."
+	# 	touch $file
+	# 	echo "$file creer"
+	# 	cd ..
+	# 	echo "rep : `pwd`"
+	# fi
+	for (( i = 0; i < 7; i++ )); do
+		rep=${tabRep[i]}
+		file=${tabFile[i]}
+
+		if [ -d $rep ]; then
+			echo "$rep existe..."
+			cd $rep
+			echo "rep : `pwd`"
+			if ! [ -e $file ]; then
+				echo "$file inexistant..."
+				echo "creation de $file..."
+				touch $file
+				echo "$file creer"
+			else
+				echo "$file deja creer"
+			fi
+			cd ..
+			echo "rep : `pwd`"
+		else
+			echo "$rep inexistant..."
+			echo "creation de $rep..."
+			mkdir $rep
+			echo "$rep creer"
+			cd $rep
+			echo "rep : `pwd`"
 			echo "$file inexistant..."
 			echo "creation de $file..."
 			touch $file
 			echo "$file creer"
-		else
-			echo "$file deja creer"
+			cd ..
+			echo "rep : `pwd`"
 		fi
-		cd ..
-		echo "rep : `pwd`"
-	else
-		echo "$rep inexistant..."
-		echo "creation de $rep..."
-		mkdir $rep
-		echo "$rep creer"
-		cd $rep
-		echo "rep : `pwd`"
-		echo "$file inexistant..."
-		echo "creation de $file..."
-		touch $file
-		echo "$file creer"
-		cd ..
-		echo "rep : `pwd`"
-	fi
-
-	rep=bin
-	file=initBin.txt
-
-	if [ -d $rep ]; then
-		echo "$rep existe..."
-		cd $rep
-		echo "rep : `pwd`"
-		if ! [ -e $file ]; then
-			echo "$file inexistant..."
-			echo "creation de $file..."
-			touch $file
-			echo "$file creer"
-		else
-			echo "$file deja creer"
-		fi
-		cd ..
-		echo "rep : `pwd`"
-	else
-		echo "$rep inexistant..."
-		echo "creation de $rep..."
-		mkdir $rep
-		echo "$rep creer"
-		cd $rep
-		echo "rep : `pwd`"
-		echo "$file inexistant..."
-		echo "creation de $file..."
-		touch $file
-		echo "$file creer"
-		cd ..
-		echo "rep : `pwd`"
-	fi
-
-	rep=build
-	file=initBuild.txt
-
-	if [ -d $rep ]; then
-		echo "$rep existe..."
-		cd $rep
-		echo "rep : `pwd`"
-		if ! [ -e $file ]; then
-			echo "$file inexistant..."
-			echo "creation de $file..."
-			touch $file
-			echo "$file creer"
-		else
-			echo "$file deja creer"
-		fi
-		cd ..
-		echo "rep : `pwd`"
-	else
-		echo "$rep inexistant..."
-		echo "creation de $rep..."
-		mkdir $rep
-		echo "$rep creer"
-		cd $rep
-		echo "rep : `pwd`"
-		echo "$file inexistant..."
-		echo "creation de $file..."
-		touch $file
-		echo "$file creer"
-		cd ..
-		echo "rep : `pwd`"
-	fi
-
-	rep=doc
-	file=initDoc.txt
-
-	if [ -d $rep ]; then
-		echo "$rep existe..."
-		cd $rep
-		echo "rep : `pwd`"
-		if ! [ -e $file ]; then
-			echo "$file inexistant..."
-			echo "creation de $file..."
-			touch $file
-			echo "$file creer"
-		else
-			echo "$file deja creer"
-		fi
-		cd ..
-		echo "rep : `pwd`"
-	else
-		echo "$rep inexistant..."
-		echo "creation de $rep..."
-		mkdir $rep
-		echo "$rep creer"
-		cd $rep
-		echo "rep : `pwd`"
-		echo "$file inexistant..."
-		echo "creation de $file..."
-		touch $file
-		echo "$file creer"
-		cd ..
-		echo "rep : `pwd`"
-	fi
-
-	rep=include
-	file=initInclude.txt
-
-	if [ -d $rep ]; then
-		echo "$rep existe..."
-		cd $rep
-		echo "rep : `pwd`"
-		if ! [ -e $file ]; then
-			echo "$file inexistant..."
-			echo "creation de $file..."
-			touch $file
-			echo "$file creer"
-		else
-			echo "$file deja creer"
-		fi
-		cd ..
-		echo "rep : `pwd`"
-	else
-		echo "$rep inexistant..."
-		echo "creation de $rep..."
-		mkdir $rep
-		echo "$rep creer"
-		cd $rep
-		echo "rep : `pwd`"
-		echo "$file inexistant..."
-		echo "creation de $file..."
-		touch $file
-		echo "$file creer"
-		cd ..
-		echo "rep : `pwd`"
-	fi
-
-	rep=lib
-	file=initLib.txt
-
-	if [ -d $rep ]; then
-		echo "$rep existe..."
-		cd $rep
-		echo "rep : `pwd`"
-		if ! [ -e $file ]; then
-			echo "$file inexistant..."
-			echo "creation de $file..."
-			touch $file
-			echo "$file creer"
-		else
-			echo "$file deja creer"
-		fi
-		cd ..
-		echo "rep : `pwd`"
-	else
-		echo "$rep inexistant..."
-		echo "creation de $rep..."
-		mkdir $rep
-		echo "$rep creer"
-		cd $rep
-		echo "rep : `pwd`"
-		echo "$file inexistant..."
-		echo "creation de $file..."
-		touch $file
-		echo "$file creer"
-		cd ..
-		echo "rep : `pwd`"
-	fi
-
-	rep=tests
-	file=initTest.txt
-
-	if [ -d $rep ]; then
-		echo "$rep existe..."
-		cd $rep
-		echo "rep : `pwd`"
-		if ! [ -e $file ]; then
-			echo "$file inexistant..."
-			echo "creation de $file..."
-			touch $file
-			echo "$file creer"
-		else
-			echo "$file deja creer"
-		fi
-		cd ..
-		echo "rep : `pwd`"
-	else
-		echo "$rep inexistant..."
-		echo "creation de $rep..."
-		mkdir $rep
-		echo "$rep creer"
-		cd $rep
-		echo "rep : `pwd`"
-		echo "$file inexistant..."
-		echo "creation de $file..."
-		touch $file
-		echo "$file creer"
-		cd ..
-		echo "rep : `pwd`"
-	fi
+	done
 	echo "creation des fichiers et repertoires terminee..."
 }
 
