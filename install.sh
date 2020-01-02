3b610e03525f5e586509289b777d648adc0309c9
Erathriel <geoffreynoro@orange.fr>
2020-01-02 15:39:01 +0100

    ajout des log de la creation de l'exe

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 #compilation et install sous linux avec exec dans bin
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=~/Desktop/Projet-R
-make VERBOSE=true install
+cmake .. -DCMAKE_INSTALL_PREFIX=~/Desktop/Projet-R > log.txt
+make VERBOSE=true install >> log.txt
