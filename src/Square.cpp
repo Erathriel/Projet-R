fe32908b4ffce2ec777fa2d78a29241d6d59a59f
Erathriel <geoffreynoro@orange.fr>
2020-01-10 11:58:58 +0100

    debug + comprehension de view plus screen

--- src/Square.cpp
+++ src/Square.cpp
@@ -2,6 +2,7 @@
 #include <gf/RenderTarget.h>
 #include <gf/Shapes.h>
 #include <gf/Unused.h>
+
 #include "../include/Square.h"
 
 Square::Square(gf::Vector2f position,float size, gf::Color4f color)
@@ -13,24 +14,39 @@ Square::Square(gf::Vector2f position,float size, gf::Color4f color)
 }
 
 void Square::update(gf::Time time){
+	printf("x : %f et y : %f\n",m_position.x,m_position.y );
 	float dt = time.asSeconds();
+	printf("dt = %f\n", dt );
 
 	switch(m_move){
 		case Move::Up:
-			m_position.y -= Velocity * dt;
+			if (m_position.y > -300.0f)
+			{
+				m_position.y -= Velocity * dt;
+			}
 			break;
 		case Move::Down:
-			m_position.y += Velocity * dt;
+			if (m_position.y < 300.0f)
+			{
+				m_position.y += Velocity * dt;
+			}
 			break;
 		case Move::Right:
-			m_position.x += Velocity * dt;
+			if (m_position.x < 200.0f)
+			{
+				m_position.x += Velocity * dt;
+			}
 			break;
 		case Move::Left:
-			m_position.x -= Velocity * dt;
+			if (m_position.x > -200.0f)
+			{
+				m_position.x -= Velocity * dt;				
+			}
 			break;
 		case Move::Stop:
 			break;
 	}
+	printf("x : %f et y : %f\n",m_position.x,m_position.y );
 }
 
 void Square::render(gf::RenderTarget& target, const gf::RenderStates& states){
